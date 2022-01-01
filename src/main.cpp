149e48d27874b5eb134f91150201407fa883997b
Eli Miller <elimiller22@gmail.com>
2022-01-01 17:50:44 -0500

    Reorganize code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 #include <ArduinoBLE.h>
-
-// variables for button
-// const int speedPin = 2;
-// int oldButtonState = LOW;
 
 // Define constants
 #define BLE_UUID "19B10000-E8F2-537E-4F6C-D104768A1214"
@@ -11,36 +7,37 @@
 #define DIR_UUID "19B10000-E8F2-537E-4F6C-D604168C1217"
 #define POT_PIN A0
 
-int potValue = 0; // variable to store the value read
-int newSpeed = 0;
-int oldSpeed = 0;
-
-
 // Function Prototypes
-void controlLed(BLEDevice peripheral);
+void controlSkateboard(BLEDevice peripheral,
+                       BLECharacteristic speedCharacteristic,
+                       BLECharacteristic forwardDirectionCharacteristic);
+bool getCharacteristics(BLEDevice peripheral,
+                        BLECharacteristic &speedCharacteristic,
+                        BLECharacteristic &forwardDirectionCharacteristic);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  // while (!Serial);
+  while (!Serial)
+    ;
 
   // initialize the BLE hardware
   BLE.begin();
 
-  Serial.println("BLE Central - skateboard control");
+  Serial.println("BLE Central - skateboard controller");
 
   // start scanning for peripherals
   BLE.scanForUuid(BLE_UUID);
 }
 
-void loop()
-{
+void loop() {
   // check if a peripheral has been discovered
   BLEDevice peripheral = BLE.available();
+  BLECharacteristic speedCharacteristic;
+  BLECharacteristic forwardDirectionCharacteristic;
 
-  if (peripheral)
-  {
-    // discovered a peripheral, print out address, local name, and advertised service
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised
+    // service
     Serial.print("Found ");
     Serial.print(peripheral.address());
     Serial.print(" '");
@@ -49,83 +46,79 @@ void loop()
     Serial.print(peripheral.advertisedServiceUuid());
     Serial.println();
 
-    if (peripheral.localName() != "SkateBoard")
-    {
+    if (peripheral.localName() != "SkateBoard") {
       return;
     }
 
     // stop scanning
     BLE.stopScan();
 
-    controlLed(peripheral);
+    if (peripheral.connect()) {
+      Serial.println("Connected to Skateboard");
+    } else {
+      Serial.println("Failed to connect to Skateboard!");
+      return;
+    }
+
+    bool characteristicsCreated = getCharacteristics(
+        peripheral, speedCharacteristic, forwardDirectionCharacteristic);
+    if (characteristicsCreated)
+      controlSkateboard(peripheral, speedCharacteristic,
+                        forwardDirectionCharacteristic);
 
     // peripheral disconnected, start scanning again
     BLE.scanForUuid(BLE_UUID);
   }
 }
 
-void controlLed(BLEDevice peripheral)
-{
-  // connect to the peripheral
-  Serial.println("Connecting ...");
-
-  if (peripheral.connect())
-  {
-    Serial.println("Connected");
-  }
-  else
-  {
-    Serial.println("Failed to connect!");
-    return;
-  }
-
+bool getCharacteristics(BLEDevice peripheral,
+                        BLECharacteristic &speedCharacteristic,
+                        BLECharacteristic &forwardDirectionCharacteristic) {
   // discover peripheral attributes
-  Serial.println("Discovering attributes ...");
-  if (peripheral.discoverAttributes())
-  {
-    Serial.println("Attributes discovered");
-  }
-  else
-  {
+  if (!peripheral.discoverAttributes()) {
     Serial.println("Attribute discovery failed!");
     peripheral.disconnect();
-    return;
+    return false;
   }
 
   // retrieve the speed characteristic
-  BLECharacteristic speedCharacteristic = peripheral.characteristic(SPEED_UUID);
+  speedCharacteristic = peripheral.characteristic(SPEED_UUID);
   // retrieve the direction characteristic
-  BLECharacteristic forwardDirectionCharacteristic = peripheral.characteristic(DIR_UUID);
+  forwardDirectionCharacteristic = peripheral.characteristic(DIR_UUID);
 
-  if (!speedCharacteristic)
-  {
+  if (!speedCharacteristic) {
     Serial.println("Peripheral does not have skateboard speed characteristic!");
     peripheral.disconnect();
-    return;
-  }
-  else if (!speedCharacteristic.canWrite())
-  {
-    Serial.println("Peripheral does not have a writable skateboard speed characteristic!");
+    return false;
+  } else if (!speedCharacteristic.canWrite()) {
+    Serial.println(
+        "Peripheral does not have a writable skateboard speed characteristic!");
     peripheral.disconnect();
-    return;
+    return false;
   }
-  if (!forwardDirectionCharacteristic)
-  {
-    Serial.println("Peripheral does not have skateboard direction characteristic!");
+  if (!forwardDirectionCharacteristic) {
+    Serial.println(
+        "Peripheral does not have skateboard direction characteristic!");
     peripheral.disconnect();
-    return;
-  }
-  else if (!forwardDirectionCharacteristic.canWrite())
-  {
-    Serial.println("Peripheral does not have a writable skateboard direction characteristic!");
+    return false;
+  } else if (!forwardDirectionCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable skateboard direction "
+                   "characteristic!");
     peripheral.disconnect();
-    return;
+    return false;
   }
-  int oldPot = 0;
+  return true;
+}
+
+void controlSkateboard(BLEDevice peripheral,
+                       BLECharacteristic speedCharacteristic,
+                       BLECharacteristic forwardDirectionCharacteristic) {
+  int potValue = 0; // variable to store the value read
+  int speedSensitivity = 5;
+  int oldSpeed = 0;
   int newSpeed = 0;
   bool forward = true;
-  while (peripheral.connected())
-  {
+  while (peripheral.connected()) {
     // while the peripheral is connected
 
     // read the pot pin
@@ -133,25 +126,28 @@ void controlLed(BLEDevice peripheral)
     newSpeed = float(potValue / 1023.0) * 100;
     newSpeed = (newSpeed - 50) * 2;
 
-    if (potValue > (oldPot + 20) || potValue < (oldPot - 20)) {
-      oldPot = potValue;
+    if (newSpeed >= (100 - speedSensitivity))
+      newSpeed = 100;
+    else if (newSpeed <= speedSensitivity && newSpeed >= -speedSensitivity)
+      newSpeed = 0;
+    else if (newSpeed <= -(100 - speedSensitivity))
+      newSpeed = -100;
+
+    if (newSpeed > (oldSpeed + speedSensitivity) ||
+        newSpeed < (oldSpeed - speedSensitivity)) {
       Serial.print("Pot Value: ");
       Serial.println(potValue);
       Serial.print("New Speed: ");
       Serial.println(newSpeed);
-    }
-    
-    if (newSpeed != oldSpeed)
-    {
       oldSpeed = newSpeed;
       if (newSpeed < 0)
         forward = false;
       else
         forward = true;
 
-
       int speedSuccess = speedCharacteristic.writeValue((int8_t)abs(newSpeed));
-      int dirSuccess = forwardDirectionCharacteristic.writeValue((int8_t)forward);
+      int dirSuccess =
+          forwardDirectionCharacteristic.writeValue((int8_t)forward);
 
       if (!speedSuccess || !dirSuccess) {
         Serial.println("Error: Value Write Failure!");
